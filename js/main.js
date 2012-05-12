f047d2f63b399b70a67f4c36469a87637604e9d1
Eli Gundry <eligundry@gmail.com>
2012-05-11 20:09:17 -0400

    Starting a new branch guys

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-/* Author:
+/* Author: Eli Gundry
 
 */
 
+jQuery(document).ready(function( $ ) {
 
+	// Ajax test
+	$.ajax({
+		dataType: "json",
+		type: "GET",
+		url: "/js/shapes.json",
+		success: function( data, textStatus, jqXHR ) {
+			$('#drawpad').drawPad('init', {
+				layers: data
+			});
+		}
+	});
 
+	$('#destroy-button').on("click", function () {
+		$('#drawpad').drawPad('destroy');
+	});
 
-
+});
